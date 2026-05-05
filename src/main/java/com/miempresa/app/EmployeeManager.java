073f59fa62a15092f3e25a8c2e8752047206dde6
Chemaxxxx <josemangt6@gmail.com>
2026-05-05 20:50:20 +0200

    Javadoc agregado

--- src/main/java/com/miempresa/app/EmployeeManager.java
+++ src/main/java/com/miempresa/app/EmployeeManager.java
@@ -3,33 +3,47 @@ package com.miempresa.app;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class EmployeeManager {
 
-    private List<Employee> employees = new ArrayList<>();
+     private List<Employee> employees = new ArrayList<>();
 
+    /**
+     * Añade un nuevo empleado al sistema.
+     *
+     * @param name nombre del empleado
+     * @param years años en la empresa
+     */
     public void addEmployee(String name, int years) {
 
         Employee employee = new Employee(name, years);
-
         employees.add(employee);
 
-        System.out.println(name + " added to the system.");
+        System.out.println(name + " añadido al sistema.");
     }
 
+    /**
+     * Muestra por pantalla la lista de empleados.
+     * Recorre la lista utilizando un bucle for-each.
+     */
     public void printEmployees() {
 
-    System.out.println("List of employees:");
+        System.out.println("Lista de empleados:");
 
-    for (Employee employee : employees) {
+        for (Employee employee : employees) {
 
-        System.out.println(
-            employee.getName()
-            + ", Years in company: "
-            + employee.getYears()
-        );
+            System.out.println(
+                employee.getName()
+                + ", Years in company: "
+                + employee.getYears()
+            );
+        }
     }
-}
 
+    /**
+     * Método principal del programa.
+     *
+     * @param args argumentos de ejecución
+     */
     public static void main(String[] args) {
 
         App manager = new App();
