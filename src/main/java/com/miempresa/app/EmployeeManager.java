8e6f02f128d83a83e3527c131c572832cd648628
Chemaxxxx <josemangt6@gmail.com>
2026-05-05 20:37:20 +0200

    Modificacion nombre clase app

---
+++ src/main/java/com/miempresa/app/EmployeeManager.java
@@ -0,0 +1,44 @@
+package com.miempresa.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    List<Employee> employees = new ArrayList<>();
+
+    public void addEmployee(String name, int years) {
+
+        Employee employee = new Employee(name, years);
+
+        employees.add(employee);
+
+        System.out.println(name + " added to the system.");
+    }
+
+    public void printEmployees() {
+
+        System.out.println("List of employees:");
+
+        for (int i = 0; i < employees.size(); i++) {
+
+            Employee employee = employees.get(i);
+
+            System.out.println(
+                employee.getName()
+                + ", Years in company: "
+                + employee.getYears()
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        App manager = new App();
+
+        manager.addEmployee("John Doe", 5);
+        manager.addEmployee("Jane Smith", 2);
+
+        manager.printEmployees();
+    }
+}
